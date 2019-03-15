388e8dd86c3a549973760bccc4c91dcf95f6489a
Joel Baranick <kadaan@users.noreply.github.com>
2019-03-14 22:54:54 -0700

    Update gitflow.rb

--- gitflow.rb
+++ gitflow.rb
@@ -12,13 +12,6 @@ class Gitflow < Formula
     end
   end
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "9e602aae685b1d0fa3db2a5d4a76b6bb73fa612dc69fe86e6571076e11d11290" => :mojave
-    sha256 "9e602aae685b1d0fa3db2a5d4a76b6bb73fa612dc69fe86e6571076e11d11290" => :high_sierra
-    sha256 "dfe4055f4e0aa48af4239d45645c920c700005556b8e1a0917d441c55e2b0a53" => :sierra
-  end
-
   head do
     url "https://github.com/kadaan/gitflow-avh.git", :branch => "develop"
 
